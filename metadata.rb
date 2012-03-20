29edf6b48c7a83799a69c93d748524f8d013c440
Chris Fordham <chris@xhost.com.au>
2012-03-20 11:45:41 +1100

    Update metadata with recipes and attributes.

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,35 @@ license          "Apache 2.0"
 description      "Installs/Configures syslog-ng"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "1.0.0"
+
+%w{ debian ubuntu centos redhat fedora }.each do |os|
+  supports os
+end
+
+recipe "syslog-ng", "Installs and configures syslog-ng."
+
+attribute "syslog-ng/user",
+  :display_name => "syslog-ng System User",
+  :description => "The syslog-ng system user.",
+  :default => "root",
+  :choice => [ 'root', 'syslog-ng' ],
+  :recipes => [ "syslog-ng::default" ]
+
+attribute "syslog-ng/group",
+  :display_name => "syslog-ng System Group",
+  :description => "The syslog-ng system group.",
+  :default => "root",
+  :choice => [ 'root', 'syslog-ng' ],
+  :recipes => [ "syslog-ng::default" ]
+  
+attribute "syslog-ng/log_dir",
+  :display_name => "syslog-ng Log Directory",
+  :description => "The syslog-ng log directory.",
+  :default => "/var/log/syslog-ng",
+  :recipes => [ "syslog-ng::default" ]
+  
+attribute "syslog-ng/config_dir",
+  :display_name => "syslog-ng Config Directory",
+  :description => "The syslog-ng configuration directory.",
+  :default => "/var/log/syslog-ng",
+  :recipes => [ "syslog-ng::default" ]
